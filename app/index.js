78f5dc72b51e7aba9edfe7bee478fc687b287a52
Dominik <dongron@wp.pl>
2017-07-15 19:32:06 +0200

    root container move to app/index.js and register for both platforms

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-    AppRegistry,
     Navigator,
     StyleSheet,
     View,
@@ -9,7 +8,7 @@ import {
     Provider,
     connect } from 'react-redux';
 
-import configureStore from './store/configureStore';
+// import configureStore from './store/configureStore';
 
 //import containers
 
@@ -25,7 +24,40 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 // import globalInitialState from './reducers/global/globalInitialState';
 // import profileInitialState from './reducers/profile/profileInitialState';
 
-
-export default function () {
-    
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Press Cmd+R to reload,{'\n'}
+                    Cmd+D or shake for dev menu
+                </Text>
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
