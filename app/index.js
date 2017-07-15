2e8d58510e1d6ca5f7472e62e0dade54c9cd1899
Dominik <dongron@wp.pl>
2017-07-15 18:39:51 +0200

    Rename app root file

---
+++ app/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    AppRegistry,
+    Navigator,
+    StyleSheet,
+    View,
+    Text } from 'react-native';
+import {
+    Provider,
+    connect } from 'react-redux';
+
+import configureStore from './store/configureStore';
+
+//import containers
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+//actions to dipatch bootstrap values
+// import {setPlatform, setVersion} from './reducers/device/deviceActions';
+// import {setStore} from './reducers/global/globalActions';
+
+//initial states
+// import authInitialState from './reducers/auth/authInitialState';
+// import deviceInitialState from './reducers/device/deviceInitialState';
+// import globalInitialState from './reducers/global/globalInitialState';
+// import profileInitialState from './reducers/profile/profileInitialState';
+
+
+export default function () {
+    
+}
