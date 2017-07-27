556ff05c39c5ca5ac9649485dcf751bdea299f5b
Dominik <dongron@wp.pl>
2017-07-27 23:49:27 +0200

    Add icons

--- app/config/images.js
+++ app/config/images.js
@@ -1,6 +1,8 @@
 const images = {
-    // avatarPlaceholder: require('./images/avatar.png'),
-    place: require('./images/place.png')
+    place: require('./images/restaurant-128.png.png'),
+    restaurant: require('./images/restaurant-128.png.png'),
+    bar: require('./images/beer-128.png'),
+
 };
 
 export default images;
