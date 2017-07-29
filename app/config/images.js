28555e27ba30923a395d076feab08b82d04739f4
Dominik <dongron@wp.pl>
2017-07-29 09:25:06 +0200

    Fixes for icons path

--- app/config/images.js
+++ app/config/images.js
@@ -1,6 +1,6 @@
 const images = {
-    place: require('./images/restaurant-128.png.png'),
-    restaurant: require('./images/restaurant-128.png.png'),
+    place: require('./images/restaurant-128.png'),
+    restaurant: require('./images/restaurant-128.png'),
     bar: require('./images/beer-128.png'),
 
 };
