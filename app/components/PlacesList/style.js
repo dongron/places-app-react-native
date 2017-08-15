1701b6d151c0acae9637ad031174eda13dd8fbf7
Dominik <dongron@wp.pl>
2017-08-15 20:29:56 +0200

    Add styles for list

--- app/components/PlacesList/style.js
+++ app/components/PlacesList/style.js
@@ -7,6 +7,21 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+    },
+    image: {
+        width: 50,
+        height: 50
+    },
+    circleIcon: {
+        fontSize: 30
+    },
+    lockIcon: {
+        fontSize: 30
+    },
+    separator: {
+        height: 1,
+        width: "100%",
+        backgroundColor: "#CED0CE"
     }
 });
 
