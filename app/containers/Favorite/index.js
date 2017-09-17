661604d3d94dc72bdafbc41105d485c6bb624749
Dominik <dongron@wp.pl>
2017-09-17 12:04:37 +0200

    Places list: remove unused code, using real data etc.

--- app/containers/Favorite/index.js
+++ app/containers/Favorite/index.js
@@ -1,67 +1,26 @@
 import {connect} from 'react-redux';
 
 import * as actions from './actions';
+import {getCurrentGeolocation} from "../../config/actions/geolocation";
+import {routeToDetailsScreen} from '../../navigators/StackNavigators/actions';
 import {itemsHasErrored, items, itemsIsLoading} from './reducer';
 import Favorite from './ui';
 import store from '../../store';
 
 
-// TODO: cloneWithRows
+//cloneWithRows no longer needed for flatlist
+//import reducers:
 const mapStateToProps = (state, props) => ({
     places: state.places,
     hasErrored: state.itemsHasErrored,
     isLoading: state.itemsIsLoading
 });
 
+//import actions:
 const mapDispatchToProps = {
-    ...actions
+    ...actions,
+    getCurrentGeolocation,
+    routeToDetailsScreen
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Favorite);
-
-
-
-
-
-
-
-// import React from 'react'
-//
-// // Navigation
-// import { addNavigationHelpers } from 'react-navigation'
-// import { NavigatorTabOne } from '../navigationConfiguration'
-//
-// // Redux
-// import { connect } from 'react-redux'
-//
-// // Icon
-// import Icon from 'react-native-vector-icons/FontAwesome'
-//
-//
-// const mapStateToProps = (state) => {
-//     return {
-//         navigationState: state.tabOne
-//     }
-// }
-//
-// class TabOneNavigation extends React.Component {
-//     static navigationOptions = {
-//         tabBarLabel: 'Tab One',
-//         tabBarIcon: ({ tintColor }) => <Icon size={ 20 } name={ 'cogs' } color={ tintColor }/>
-//     }
-//
-//     render(){
-//         const { navigationState, dispatch } = this.props
-//         return (
-//             <NavigatorTabOne
-//                 navigation={
-//                     addNavigationHelpers({
-//                         dispatch: dispatch,
-//                         state: navigationState
-//                     })
-//                 }
-//             />
-//         )
-//     }
-// }
-// export default connect(mapStateToProps)(TabOneNavigation)
--- app/containers/Favorite/ui.js
+++ app/containers/Favorite/ui.js
@@ -21,100 +21,16 @@ export default class Favorite extends Component {
     };
 
     places = this.props.places || [];
-
-    listData = [{
-        name: "Cafe S",
-        street: "Sobieskiego",
-        streetNr: "1",
-        localNr: "110",
-        city: "Częstochowa",
-        geo: {
-            latitude: 66,
-            longitude: 66
-        },
-        ownerEmail: "w@w.w",
-        schedule: {
-            "1-o": new Date('2015-01-01'),
-            "1-c": new Date('2020-01-01'),
-            "2-o": new Date('2015-01-01'),
-            "2-c": new Date('2020-01-01'),
-            "3-o": new Date('2015-01-01'),
-            "3-c": new Date('2020-01-01'),
-            "4-o": new Date('2015-01-01'),
-            "4-c": new Date('2020-01-01'),
-            "5-o": new Date('2015-01-01'),
-            "5-c": new Date('2020-01-01'),
-            "6-o": new Date('2015-01-01'),
-            "6-c": new Date('2020-01-01'),
-            "0-o": new Date('2015-01-01'),
-            "0-c": new Date('2020-01-01'),
-        },
-        clientsVelocity: {
-            current: 900,
-            max: 1000,
-            maxGroup: 25
-        }
-    },
-    {
-        name: "C 2",
-        street: "Sokiego",
-        streetNr: "99",
-        localNr: "9999",
-        city: "Wawa",
-        geo: {
-            latitude: 22,
-            longitude: 22
-        },
-        ownerEmail: "e@e.w",
-        schedule: {
-            "1-o": new Date('2015-01-01'),
-            "1-c": new Date('2015-06-06'),
-            "2-o": new Date('2015-01-01'),
-            "2-c": new Date('2015-06-06'),
-            "3-o": new Date('2015-01-01'),
-            "3-c": new Date('2015-06-06'),
-            "4-o": new Date('2015-01-01'),
-            "4-c": new Date('2015-06-06'),
-            "5-o": new Date('2015-01-01'),
-            "5-c": new Date('2015-06-06'),
-            "6-o": new Date('2015-01-01'),
-            "6-c": new Date('2015-06-06'),
-            "0-o": new Date('2015-01-01'),
-            "0-c": new Date('2015-06-06'),
-        },
-        clientsVelocity: {
-            current: 0,
-            max: 30,
-            maxGroup: 8
-        },
-    }];
-
     currentLocation;
-
-    onItemClick(item) {
-        console.log('item in parent clicked', item);
-    }
 
 
     constructor(props) {
         super(props);
-        // this.listData = props;
     }
 
     componentDidMount() {
-        // this.getCurrentPosition();
+        this.getCurrentPosition();
         this.getNearbyPlacesFromApi();
-    }
-
-    getCurrentPosition() {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                let initialPosition = JSON.stringify(position);
-                this.currentLocation = initialPosition;
-            },
-            (error) => alert(error.message),
-            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-        );
     }
 
     getNearbyPlacesFromApi() {
@@ -122,14 +38,16 @@ export default class Favorite extends Component {
         getNearbyPlaces();
     }
 
-
-    onItemPress(item) {
-        console.warn(item, 'item in parent!');
+    getCurrentPosition() {
+        const {getCurrentGeolocation} = this.props;
+        console.log('props', this.props);  
+        getCurrentGeolocation();
     }
+
 
     render() {
         return(
-            <PlacesList listData={this.props.places} position={this.currentLocation} onItemPress={this.onItemPress} />
+            <PlacesList listData={this.props.places} position={this.currentLocation} onItemPress={this.props.routeToDetailsScreen} />
         );
     }
 }
